952bc20a3b8330d3ecbcc51e0a38114b8347f6ae
Alberto S. Castaneda <alb2594@gmail.com>
2016-05-08 15:16:02 -0600

    Menu de tarifa, solicitante

--- app/src/main/java/gr23/fia/ues/sv/admon_poli/TarifaMenuActivity.java
+++ app/src/main/java/gr23/fia/ues/sv/admon_poli/TarifaMenuActivity.java
@@ -1,13 +1,40 @@
 package gr23.fia.ues.sv.admon_poli;
 
+import android.app.ListActivity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class TarifaMenuActivity extends AppCompatActivity {
+public class TarifaMenuActivity extends ListActivity {
+
+    String[] menu = {"Insertar Tarifa","Eliminar Tarifa","Consultar Tarifa", "Actualizar Tarifa"};
+    String[] activities = {"TarifaInsertarActivity","TarifaEliminarActivity","TarifaConsultarActivity",
+            "TarifaActualizarActivity"};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tarifa_menu);
+        ListView listView = getListView();
+        listView.setBackgroundColor(Color.rgb(0, 0, 255));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, menu);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id){
+        super.onListItemClick(l, v, position, id);
+        String nombreValue=activities[position];
+        l.getChildAt(position).setBackgroundColor(Color.rgb(128, 128, 255));
+        try{
+            Class<?> clase=Class.forName("gr23.fia.ues.sv.admon_poli."+nombreValue);
+            Intent inte = new Intent(this,clase);
+            this.startActivity(inte);
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
     }
 }
