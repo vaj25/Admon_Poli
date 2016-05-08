3611add2a79071b4825d59a09aef1a36fdb025d2
Alberto S. Castaneda <alb2594@gmail.com>
2016-05-08 17:29:52 -0600

    android manifest

--- app/src/main/java/gr23/fia/ues/sv/admon_poli/SolicitanteInsertarActivity.java
+++ app/src/main/java/gr23/fia/ues/sv/admon_poli/SolicitanteInsertarActivity.java
@@ -3,11 +3,11 @@ package gr23.fia.ues.sv.admon_poli;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SolicitanteInsetarActivity extends AppCompatActivity {
+public class SolicitanteInsertarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_solicitante_insetar);
+        setContentView(R.layout.activity_solicitante_insertar);
     }
 }
