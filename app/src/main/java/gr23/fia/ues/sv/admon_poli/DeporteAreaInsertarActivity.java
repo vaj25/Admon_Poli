036b6ef74aece05d52ecbb34c7e4e7008288bccd
moisesHerrera01 <moises.oct@gmail.com>
2016-05-08 14:48:24 -0600

    Añadidos archivos Activity

---
+++ app/src/main/java/gr23/fia/ues/sv/admon_poli/DeporteAreaInsertarActivity.java
@@ -0,0 +1,13 @@
+package gr23.fia.ues.sv.admon_poli;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeporteAreaInsertarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deporte_area_insertar);
+    }
+}
