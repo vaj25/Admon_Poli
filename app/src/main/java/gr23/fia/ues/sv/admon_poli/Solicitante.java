5b780a88a5b349ea5996b5b3d80d175674a45ebf
Alberto S. Castaneda <alb2594@gmail.com>
2016-05-08 11:43:39 -0600

    Activities de clases

---
+++ app/src/main/java/gr23/fia/ues/sv/admon_poli/Solicitante.java
@@ -0,0 +1,7 @@
+package gr23.fia.ues.sv.admon_poli;
+
+/**
+ * Created by FAMILY on 08/05/2016.
+ */
+public class Solicitante {
+}
---
+++ app/src/main/java/gr23/fia/ues/sv/admon_poli/SolicitudConsultarActivity.java
@@ -0,0 +1,13 @@
+package gr23.fia.ues.sv.admon_poli;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SolicitudConsultarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solicitud_consultar);
+    }
+}
