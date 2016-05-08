5fc7241fccd92a2c7effb137a176c56f81e9f4c4
pjtobar <pablo.tobar711@gmail.com>
2016-05-08 09:12:54 -0600

    pablo: reserva,detalleReserva, horario, y parte en controlBD

---
+++ app/src/main/java/gr23/fia/ues/sv/admon_poli/DetalleReserva.java
@@ -0,0 +1,34 @@
+package gr23.fia.ues.sv.admon_poli;
+
+/**
+ * Created by User on 08/05/2016.
+ */
+public class DetalleReserva {
+
+
+    private int idReserva;
+    private int idArea;
+    private
+
+    public DetalleReserva(int idReserva, int idArea){
+        this.idReserva = idReserva;
+        this.idArea =  idArea;
+    }
+
+    public int getIdReserva() {
+        return idReserva;
+    }
+
+    public void setIdReserva(int idReserva) {
+        this.idReserva = idReserva;
+    }
+
+    public int getIdArea() {
+        return idArea;
+    }
+
+    public void setIdArea(int idArea) {
+        this.idArea = idArea;
+    }
+
+}
