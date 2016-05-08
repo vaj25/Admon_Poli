808e378768331d8b5f3c7e4f4bd3a6331b9a4f7f
Alberto S. Castaneda <alb2594@gmail.com>
2016-05-07 23:46:21 -0600

    Se crean las clases del proyecto area y deporte

--- app/src/main/java/gr23/fia/ues/sv/admon_poli/Area.java
+++ app/src/main/java/gr23/fia/ues/sv/admon_poli/Area.java
@@ -4,4 +4,48 @@ package gr23.fia.ues.sv.admon_poli;
  * Created by FAMILY on 07/05/2016.
  */
 public class Area {
+
+    private int idArea ;
+    private String nombre ;
+    private int capacidad ;
+    private float area ; // esta area es en mt2
+
+    public Area(int idArea, String nombre, int capacidad, float area) {
+        this.idArea = idArea;
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+        this.area = area;
+    }
+
+    public int getIdArea() {
+        return idArea;
+    }
+
+    public void setIdArea(int idArea) {
+        this.idArea = idArea;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public float getArea() {
+        return area;
+    }
+
+    public void setArea(float area) {
+        this.area = area;
+    }
 }
