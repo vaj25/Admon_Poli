8bf53e21c1f370b654bfb2c469d46431124a88d7
pjtobar <pablo.tobar711@gmail.com>
2016-05-08 15:56:07 -0600

    menu principal, submenu horario,reserva,detallereserva

--- app/src/main/java/gr23/fia/ues/sv/admon_poli/MainActivity.java
+++ app/src/main/java/gr23/fia/ues/sv/admon_poli/MainActivity.java
@@ -1,13 +1,33 @@
 package gr23.fia.ues.sv.admon_poli;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+public class MainActivity extends ListActivity {
+    String[] menu={"Tabla Actividad","Tabla Administrador","Tabla Area","Tabla Deporte","Tabla DeporteArea","Tabla DetalleReserva","Tabla DetalleSolicitud","Tabla Horario","Tabla Reserva","Tabla Solicitante","Tabla Solicitud","Tabla Tarifa","LLenar Base de Datos"};
+    String[] activities={"ActividadMenuActivity","AdministradorMenuActivity","AreaMenuActivity","DeporteMenuActivity","DeporteAreaMenuActivity","DetalleReservaMenuActivity","DetalleSolicitudMenuActivity","HorarioMenuActivity","ReservaMenuActivity","SolicitanteMenuActivity","SolicitudMenuActivity","TarifaMenuActivity"};
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu));
+    }
+    @Override
+    protected void onListItemClick(ListView l,View v,int position,long id){
+        super.onListItemClick(l, v, position, id);
+        if(position!=12){
+            String nombreValue=activities[position];
+            try{
+                Class<?> clase=Class.forName("gr23.fia.ues.sv.admon_poli."+nombreValue);
+                Intent inte = new Intent(this,clase);
+                this.startActivity(inte);
+            }catch(ClassNotFoundException e){
+                e.printStackTrace();
+            }
+        }else{
+//CODIGO PARA LLENAR BASE DE DATOS
+        }
     }
 }
