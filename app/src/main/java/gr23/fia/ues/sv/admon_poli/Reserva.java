7efbff39066eda40d9e653772d808ad996efce27
pjtobar <pablo.tobar711@gmail.com>
2016-05-08 01:14:54 -0600

    clase horario reserva

--- app/src/main/java/gr23/fia/ues/sv/admon_poli/Reserva.java
+++ app/src/main/java/gr23/fia/ues/sv/admon_poli/Reserva.java
@@ -4,4 +4,28 @@ package gr23.fia.ues.sv.admon_poli;
  * Created by FAMILY on 07/05/2016.
  */
 public class Reserva {
+    private String fechaReserva;
+    private Double tiempoReserva;
+
+    public Reserva(String fechaReserva, Double tiempoReserva){
+        this.fechaReserva = fechaReserva;
+        this.tiempoReserva = tiempoReserva;
+    }
+
+    public String getFechaReserva(){
+        return fechaReserva;
+    }
+    public void setFechaReserva(String fechaReserva) {
+        this.fechaReserva = fechaReserva;
+    }
+
+    public Double getTiempoReserva() {
+        return tiempoReserva;
+    }
+
+    public void setTiempoReserva(Double tiempoReserva) {
+        this.tiempoReserva = tiempoReserva;
+    }
+
+
 }
