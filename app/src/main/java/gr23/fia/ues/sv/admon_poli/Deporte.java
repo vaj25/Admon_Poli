28c1b7ecd96f1f0cdcddbe2cdb56869620834646
Alberto S. Castaneda <alb2594@gmail.com>
2016-05-07 23:24:51 -0600

    Se crean las clases del proyecto

--- app/src/main/java/gr23/fia/ues/sv/admon_poli/Deporte.java
+++ app/src/main/java/gr23/fia/ues/sv/admon_poli/Deporte.java
@@ -4,4 +4,28 @@ package gr23.fia.ues.sv.admon_poli;
  * Created by FAMILY on 07/05/2016.
  */
 public class Deporte {
+
+    private int idDeporte ;
+    private String nombre ;
+
+    public Deporte(int idDeporte, String nombre) {
+        this.idDeporte = idDeporte;
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getIdDeporte() {
+        return idDeporte;
+    }
+
+    public void setIdDeporte(int idDeporte) {
+        this.idDeporte = idDeporte;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
