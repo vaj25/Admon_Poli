3b638a378d065170f6c6cf50635de6d6e00526d3
moisesHerrera01 <moises.oct@gmail.com>
2016-05-08 18:56:45 -0600

    Submenús Deporte y DeporteArea

--- app/src/main/java/gr23/fia/ues/sv/admon_poli/AreaMenuActivity.java
+++ app/src/main/java/gr23/fia/ues/sv/admon_poli/AreaMenuActivity.java
@@ -17,7 +17,7 @@ public class AreaMenuActivity extends ListActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ListView listView = getListView();
-        listView.setBackgroundColor(Color.rgb(0, 0, 255));
+        listView.setBackgroundColor(Color.rgb(255, 255, 255));
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, menu);
         setListAdapter(adapter);
     }
--- app/src/main/java/gr23/fia/ues/sv/admon_poli/DeporteAreaMenuActivity.java
+++ app/src/main/java/gr23/fia/ues/sv/admon_poli/DeporteAreaMenuActivity.java
@@ -1,13 +1,38 @@
 package gr23.fia.ues.sv.admon_poli;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class DeporteAreaMenuActivity extends AppCompatActivity {
+public class DeporteAreaMenuActivity extends ListActivity {
+
+    String[] menu={"Insertar DeporteArea","Eliminar DeporteArea","Consultar DeporteArea", "Actualizar DeporteArea"};
+    String[] activities={"DeporteAreaInsertarActivity","DeporteAreaEliminarActivity","DeporteAreaConsultarActivity", "DeporteAreaActualizarActivity"};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_deporte_area_menu);
+        ListView listView = getListView();
+        listView.setBackgroundColor(Color.rgb(255, 255, 255));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, menu);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l,View v,int position,long id){
+        super.onListItemClick(l, v, position, id);
+        String nombreValue=activities[position];
+        l.getChildAt(position).setBackgroundColor(Color.rgb(0, 128, 64));
+        try{
+            Class<?> clase=Class.forName("gr23.fia.ues.sv.admon_poli."+nombreValue);
+            Intent inte = new Intent(this,clase);
+            this.startActivity(inte);
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
     }
 }
