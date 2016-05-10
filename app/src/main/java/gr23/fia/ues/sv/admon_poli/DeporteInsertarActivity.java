f9974aeecf3e929e0e9e300df4ce1d14d6af2e27
Alberto S. Castaneda <alb2594@gmail.com>
2016-05-10 11:41:10 -0600

    se arregla algunos layout y insertar deporte

--- app/src/main/java/gr23/fia/ues/sv/admon_poli/DeporteInsertarActivity.java
+++ app/src/main/java/gr23/fia/ues/sv/admon_poli/DeporteInsertarActivity.java
@@ -5,28 +5,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
 public class DeporteInsertarActivity extends Activity {
+
     ControlBD helper;
     EditText IdDeporte;
     EditText NombreDeporte;
+
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState); setContentView(R.layout.activity_deporte_insertar);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deporte_insertar);
         helper = new ControlBD(this);
         IdDeporte = (EditText) findViewById(R.id.IdDeporte);
         NombreDeporte = (EditText) findViewById(R.id.NombreDeporte);
     }
+
     public void insertarDeporte(View v) {
         String iddeporte=IdDeporte.getText().toString();
         String nombre=NombreDeporte.getText().toString();
         String regInsertados;
         Deporte deporte=new Deporte();
-        deporte.setIdDeporte(Integer.getInteger(iddeporte));
+        deporte.setIdDeporte(Integer.parseInt(iddeporte));
         deporte.setNombre(nombre);
         helper.abrir();
         regInsertados=helper.insertar(deporte);
         helper.cerrar();
         Toast.makeText(this, regInsertados, Toast.LENGTH_SHORT).show();
     }
+
     public void limpiarTexto(View v) {
         IdDeporte.setText("");
         NombreDeporte.setText("");
