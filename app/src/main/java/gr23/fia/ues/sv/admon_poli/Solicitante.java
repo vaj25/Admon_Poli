0d68dedddd76f29eb01c9eec30565b73325b5e8e
samueljovel8 <samueljovel8@gmail.com>
2016-05-09 21:17:17 -0600

    crud de clases solicitante

--- app/src/main/java/gr23/fia/ues/sv/admon_poli/Solicitante.java
+++ app/src/main/java/gr23/fia/ues/sv/admon_poli/Solicitante.java
@@ -4,4 +4,57 @@ package gr23.fia.ues.sv.admon_poli;
  * Created by FAMILY on 08/05/2016.
  */
 public class Solicitante {
+    private int dui;
+    private String nombreSol;
+    private String apellidoSol;
+    private int telefonoSol;
+    private String mail;
+
+    public Solicitante(int dui, String nombreSol, String apellidoSol, int telefonoSol, String mail) {
+        this.dui = dui;
+        this.nombreSol = nombreSol;
+        this.apellidoSol = apellidoSol;
+        this.telefonoSol = telefonoSol;
+        this.mail = mail;
+    }
+
+    public int getDui() {
+        return dui;
+    }
+
+    public void setDui(int dui) {
+        this.dui = dui;
+    }
+
+    public String getNombreSol() {
+        return nombreSol;
+    }
+
+    public void setNombreSol(String nombreSol) {
+        this.nombreSol = nombreSol;
+    }
+
+    public String getApellidoSol() {
+        return apellidoSol;
+    }
+
+    public void setApellidoSol(String apellidoSol) {
+        this.apellidoSol = apellidoSol;
+    }
+
+    public int getTelefonoSol() {
+        return telefonoSol;
+    }
+
+    public void setTelefonoSol(int telefonoSol) {
+        this.telefonoSol = telefonoSol;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
 }
--- app/src/main/java/gr23/fia/ues/sv/admon_poli/Tarifa.java
+++ app/src/main/java/gr23/fia/ues/sv/admon_poli/Tarifa.java
@@ -4,4 +4,37 @@ package gr23.fia.ues.sv.admon_poli;
  * Created by FAMILY on 07/05/2016.
  */
 public class Tarifa {
+    public Double montoArea;
+    public double canthora;
+    public int cantPersonas;
+
+    public Tarifa(Double montoArea, double canthora, int cantPersonas) {
+        this.montoArea = montoArea;
+        this.canthora = canthora;
+        this.cantPersonas = cantPersonas;
+    }
+
+    public Double getMontoArea() {
+        return montoArea;
+    }
+
+    public void setMontoArea(Double montoArea) {
+        this.montoArea = montoArea;
+    }
+
+    public double getCanthora() {
+        return canthora;
+    }
+
+    public void setCanthora(double canthora) {
+        this.canthora = canthora;
+    }
+
+    public int getCantPersonas() {
+        return cantPersonas;
+    }
+
+    public void setCantPersonas(int cantPersonas) {
+        this.cantPersonas = cantPersonas;
+    }
 }
