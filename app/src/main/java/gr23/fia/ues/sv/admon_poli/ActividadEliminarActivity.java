b9a2da49632e9d7b4c6c27c4e661b0b6bfd05a51
Karla-V <susume07@gmail.com>
2016-05-10 23:28:26 -0600

    actividad insertar y eliminar activity

--- app/src/main/java/gr23/fia/ues/sv/admon_poli/ActividadEliminarActivity.java
+++ app/src/main/java/gr23/fia/ues/sv/admon_poli/ActividadEliminarActivity.java
@@ -1,13 +1,30 @@
 package gr23.fia.ues.sv.admon_poli;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class ActividadEliminarActivity extends AppCompatActivity {
-
+public class ActividadEliminarActivity extends Activity {
+    EditText editId;
+    ControlBD controlhelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_eliminar);
+        controlhelper= new ControlBD(this);
+        editId=(EditText)findViewById(R.id.editIdEliminar);
+    }
+
+    public void eliminarActividad(View v){
+        String regEliminadas;
+        Actividad actividad= new Actividad();
+        actividad.setIdActividad(Integer.parseInt(editId.getText().toString()));
+        controlhelper.abrir();
+        regEliminadas=controlhelper.eliminar(actividad);
+        controlhelper.cerrar();
+        Toast.makeText(this, regEliminadas, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/gr23/fia/ues/sv/admon_poli/ActividadInsertarActivity.java
+++ app/src/main/java/gr23/fia/ues/sv/admon_poli/ActividadInsertarActivity.java
@@ -1,13 +1,41 @@
 package gr23.fia.ues.sv.admon_poli;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class ActividadInsertarActivity extends AppCompatActivity {
+public class ActividadInsertarActivity extends Activity {
 
+    ControlBD helper;
+    EditText editIdActividad;
+    EditText editNombreActividad;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_insertar);
+        helper = new ControlBD(this);
+        editIdActividad = (EditText) findViewById(R.id.editIdActividad);
+        editNombreActividad = (EditText) findViewById(R.id.editNombreActividad);
+    }
+
+    public void insertarActividad(View v){
+        int idActividad = Integer.parseInt(editIdActividad.getText().toString());
+        String nombreActividad=editNombreActividad.getText().toString();
+        String regInsertados;
+        Actividad actividad=new Actividad();
+        actividad.setIdActividad(idActividad);
+        actividad.setNombreactividad(nombreActividad);
+        helper.abrir();
+        regInsertados=helper.insertar(actividad);
+        helper.cerrar();
+        Toast.makeText(this, regInsertados, Toast.LENGTH_SHORT).show();
+    }
+
+    public void limpiarTexto(View v){
+        editIdActividad.setText("");
+        editNombreActividad.setText("");
     }
 }
