0b90577687a1d538301959d0d9aaf04fbb9230f2
moisesHerrera01 <moises.oct@gmail.com>
2016-05-14 22:07:34 -0600

    añadido espinner eliminar detalleSolicitud

--- app/src/main/java/gr23/fia/ues/sv/admon_poli/DetalleSolicitudEliminarActivity.java
+++ app/src/main/java/gr23/fia/ues/sv/admon_poli/DetalleSolicitudEliminarActivity.java
@@ -1,38 +1,76 @@
 package gr23.fia.ues.sv.admon_poli;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class DetalleSolicitudEliminarActivity extends Activity {
-
-    EditText idSolicitud;
-    EditText idArea;
     ControlBD helper;
-
+    Spinner sSolicitud;
+    Spinner sArea;
+    List<Area> lista;
+    List<Solicitud> lista1;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+        helper = new ControlBD(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_solicitud_eliminar);
+        sSolicitud = (Spinner) findViewById(R.id.selectSolicitud);
+        sArea = (Spinner) findViewById(R.id.selectArea);
+        helper=new ControlBD (this);
 
-        helper = new ControlBD (this);
-        idArea = (EditText)findViewById(R.id.editIdArea);
-        idSolicitud = (EditText)findViewById(R.id.editIdSolicitud);
+        lista = new ArrayList<>();
+        lista=helper.consultaArea();
+        ArrayAdapter<Area> adaptador =new ArrayAdapter<Area>(this,android.R.layout.simple_spinner_item,lista);
+        adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sArea.setAdapter(adaptador);
+
+        lista1 =new ArrayList<>();
+        lista1=helper.consultaSolicitud();
+        ArrayAdapter<Solicitud> adaptador1 =new ArrayAdapter<Solicitud>(this,android.R.layout.simple_spinner_item,lista1);
+        adaptador1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sSolicitud.setAdapter(adaptador1);
     }
 
     public void eliminarDetalleSolicitud(View v){
+        DetalleSolicitud ds= new DetalleSolicitud();
+        int position= sArea.getSelectedItemPosition();
+        int position1= sSolicitud.getSelectedItemPosition();
+        Iterator iterador = lista.listIterator();
+        Iterator iterador1 = lista1.listIterator();
+
+        int count=0;
+        int idarea=0;
+        int idsolicitud=0;
+        while( iterador.hasNext() ) {
+            Area area = (Area) iterador.next();
+            if(count==position){
+                idarea=area.getIdArea();
+            }
+            count++;
+        }
+        int count1=0;
+        while( iterador1.hasNext() ) {
+            Solicitud sol = (Solicitud) iterador1.next();
+            if(count1==position1){
+                idsolicitud=sol.getIdSolicitud();
+            }
+            count1++;
+        }
         String regEliminadas;
-        DetalleSolicitud ds = new DetalleSolicitud();
-        String idsolicitud = idSolicitud.getText().toString();
-        String idarea = idArea.getText().toString();
-        ds.setIdSolicitud(Integer.parseInt(idsolicitud));
-        ds.setIdArea(Integer.parseInt(idarea));
+        ds.setIdArea(idarea);
+        ds.setIdSolicitud(idsolicitud);
         helper.abrir();
-        regEliminadas = helper.eliminar(ds);
+        regEliminadas=helper.eliminar(ds);
         helper.cerrar();
         Toast.makeText(this, regEliminadas, Toast.LENGTH_SHORT).show();
     }
 }
+
