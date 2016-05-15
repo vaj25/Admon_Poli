4768275df4632f2b47acd897f7af791e0a52431b
Alberto S. Castaneda <alb2594@gmail.com>
2016-05-15 00:59:09 -0600

    crud solicitud con spinne

--- app/src/main/java/gr23/fia/ues/sv/admon_poli/SolicitudEliminarActivity.java
+++ app/src/main/java/gr23/fia/ues/sv/admon_poli/SolicitudEliminarActivity.java
@@ -5,14 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
 public class SolicitudEliminarActivity extends Activity {
 
-    EditText idSolicitud ;
+    Spinner idSolicitud;
+    List<Solicitud> lista ;
     ControlBD helper ;
 
     @Override
@@ -20,14 +24,32 @@ public class SolicitudEliminarActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solicitud_eliminar);
         helper = new ControlBD(this);
-        idSolicitud = (EditText) findViewById(R.id.txtIdSolicitud) ;
+        idSolicitud = (Spinner) findViewById(R.id.selectSolicitud) ;
+
+        lista =new ArrayList<>();
+        lista=helper.consultaSolicitud();
+        ArrayAdapter<Solicitud> adaptador =new ArrayAdapter<Solicitud>(this,android.R.layout.simple_spinner_item,lista);
+        adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        idSolicitud.setAdapter(adaptador);
     }
 
     public void eliminarSolicitud(View v){
+
+        int position = idSolicitud.getSelectedItemPosition();
+        Iterator iterador = lista.listIterator();
+        int count=0;
+        int idsolicitud=0;
+        while(iterador.hasNext() ) {
+            Solicitud soli = (Solicitud) iterador.next();
+            if(count==position){
+                idsolicitud=soli.getIdSolicitud();
+            }
+            count++;
+        }
+
         String regEliminadas;
         Solicitud solicitud = new Solicitud();
-        String idsolicitud = idSolicitud.getText().toString();
-        solicitud.setIdSolicitud(Integer.parseInt(idsolicitud));
+        solicitud.setIdSolicitud(idsolicitud);
         helper.abrir();
         regEliminadas = helper.eliminar(solicitud);
         helper.cerrar();
